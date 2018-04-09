86daa0a0e0fbe26402665ca13c8fe141bfb9eb09
rajeshneti <ee15btech11021@iith.ac.in>
2018-04-09 14:39:27 +0530

    removed unnecessary files

--- src/site_modelling/test.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: rajeshneti
-# @Date:   2018-04-02 12:58:55
-# @Last Modified by:   rajeshneti
-# @Last Modified time: 2018-04-02 12:59:42
-import wikipedia 
-print wikipedia.summary("Wikipedia")
